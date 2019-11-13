d2dc7ac2c11e40960ec151fa2862c105dede4335
Leverkusen <l_baec02@uni-muenster.de>
2019-11-13 10:46:46 +0100

    DWD Warning-Layer

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -46,10 +46,9 @@ function map(){
     var start = L.marker([52.26524,7.72767]).bindPopup('This is the Startpoint');
     var excample = L.layerGroup([start]);
 
-// Warnungs-Layer vom DWD-Geoserver
-/**
-	var warnlayer = L.tileLayer("https://maps.dwd.de/geoproxy_warnungen/service/", {
-		layers: 'Warnungen_Gemeinden_vereinigt',
+	// Warnungs-Layer vom DWD-Geoserver - betterWms fügt Möglichkeiten zur GetFeatureInfo hinzu
+	var warnlayer = L.tileLayer.betterWms("https://maps.dwd.de/geoproxy_warnungen/service/", {
+		layers: 'Warnungen_Landkreise',
 		// eigene Styled Layer Descriptor (SLD) können zur alternativen Anzeige der Warnungen genutzt werden (https://docs.geoserver.org/stable/en/user/styling/sld/reference/)
 		//sld: 'https://eigenerserver/alternativer.sld',
 		format: 'image/png',
@@ -58,41 +57,27 @@ function map(){
 		attribution: 'Warndaten: &copy; <a href="https://www.dwd.de">DWD</a>'
 	});
 
-// Niederschlags-Layer vom DWD-Geoserver
-	var niederschlagslayer = L.tileLayer("https://maps.dwd.de/geoserver/dwd/ows?service=WMS&version=1.3&request=GetMap&layers=dwd:RX-Produkt&srs=EPSG:4326&format=image/jpeg", {
-		attribution: 'Niederschlagsradar: &copy; <a href="https://www.dwd.de">DWD</a>'
+	// CQL_FILTER können benutzt werden um angezeigte Warnungen zu filtern (https://docs.geoserver.org/stable/en/user/tutorials/cql/cql_tutorial.html)
+	// Filterung kann auf Basis der verschiedenen properties der Warnungen erfolgen (bspw. EC_II, EC_GROUP, DESCRIPTION ... ) siehe https://www.dwd.de/DE/wetter/warnungen_aktuell/objekt_einbindung/einbindung_karten_geowebservice.pdf
+ 	// warnlayer.setParams({CQL_FILTER:"DESCRIPTION LIKE '%Sturm%'"});
+ 	// Filter können zur Laufzeit, z.B. über Nutzereingaben angepasst werden
+ 	//delete warnlayer.wmsParams.CQL_FILTER;
+ 	//warnlayer.redraw();
+
+ 	// Layer mit neutraler Darstellung der Gemeinde-Warngebiete
+	var gemeindelayer = L.tileLayer.wms("https://maps.dwd.de/geoproxy_warnungen/service/", {
+		layers: 'Warngebiete_Gemeinden',
+		format: 'image/png',
+		styles: '',
+		transparent: true,
+		opacity: 0.6,
+		attribution: 'Geobasisdaten Gemeinden: &copy; <a href="https://www.bkg.bund.de">BKG</a> 2015 (Daten verändert)'
 	});
-
-
-
-	// Warnungs-Layer pro Kreis vom DWD-Geoserver
-    	var warnlayer_kreis = L.tileLayer("https://maps.dwd.de/geoproxy_warnungen/service/", {
-    		layers: 'dwd%3AWarngebiete_Kreise',
-    		// eigene Styled Layer Descriptor (SLD) können zur alternativen Anzeige der Warnungen genutzt werden (https://docs.geoserver.org/stable/en/user/styling/sld/reference/)
-    		//sld: 'https://eigenerserver/alternativer.sld',
-    		format: 'image/png',
-    		transparent: true,
-    		opacity: 0.8,
-    		attribution: 'Warndaten: &copy; <a href="https://www.dwd.de">DWD</a>'
-    	});
-
-	// Warnungs-Layer pro Kreis vom DWD-Geoserver
-    	var wind_warn = L.TileLayer.WMS("https://maps.dwd.de/geoserver/dwd/ows?", {
-    	version: 1.3,
-    	layers: 'dwd:Warnungen_Landkreise',
-        transparent: true,
-        opacity: 0.8,
-    	attribution: 'Warndaten: &copy; <a href="https://www.dwd.de">DWD</a>'
-    	})
-
-**/
 
     var overlayMaps = {
         "Excample": excample,
-        //"DWD Warnings": warnlayer,
-        //"DWD Niederschlag": niederschlagslayer,
-        //"DWD Wind": wind_warn,
-        //"DWD Kreis-Warning": warnlayer_kreis
+        "DWD Warnings": warnlayer,
+        "Gemeindegrenzen": gemeindelayer,
     };
 
     L.control.layers(baseMaps, overlayMaps).addTo(map);
