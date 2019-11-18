355660b4d320694aed251ca6ff3183280d24f017
m-gado01 <m_gado01@uni-muenster.de>
2019-11-18 15:51:32 +0100

    Test push

--- app.js
+++ app.js
@@ -10,46 +10,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var validator = require('express-validator');
 var flash = require('express-flash');
 var session = require('express-session');
 var JL = require('jsnlog').JL;
 var jsnlog_nodejs = require('jsnlog-nodejs').jsnlog_nodejs;
+/* var mongoose = require('mongoose'); */
 
-
-var config = require('./config/database');
-
-function connectMongoDB() {
-  (async () => {
-    // set up default ("Docker") mongoose connection
-    await mongoose.connect(config.databaseDocker, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      autoReconnect: true
-    }).then(db => {
-      console.log('Connected to MongoDB (databasename: "'+db.connections[0].name+'") on host "'+db.connections[0].host+'" and on port "'+db.connections[0].port+'""');
-    }).catch(async err => {
-      console.log('Connection to '+config.databaseDocker+' failed, try to connect to '+config.databaseLocal);
-      // set up "local" mongoose connection
-      await mongoose.connect(config.databaseLocal, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        autoReconnect: true
-      }).then(db => {
-        console.log('Connected to MongoDB (databasename: "'+db.connections[0].name+'") on host "'+db.connections[0].host+'" and on port "'+db.connections[0].port+'""');
-      }).catch(err2nd => {
-        console.log('Error at MongoDB-connection with Docker: '+err);
-        console.log('Error at MongoDB-connection with Localhost: '+err2nd);
-        console.log('Retry to connect in 3 seconds');
-        setTimeout(connectMongoDB, 3000); // retry until db-server is up
-      });
-    });
-  })();
-}
-// connect to MongoDB
-connectMongoDB();
+var token = require('./config/token');
+/* var config = require('./config/database'); */
 
 var app = express();
 
@@ -63,6 +33,7 @@ app.use('/', express.static(__dirname + '/public'));
 // make packages available for client using statics
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use('/mdbootstrap', express.static(__dirname + '/node_modules/mdbootstrap'));
 app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist'));
 app.use('/open-iconic', express.static(__dirname + '/node_modules/open-iconic/font'));
 app.use("/leaflet", express.static(__dirname + "/node_modules/leaflet/dist"));
@@ -91,26 +62,24 @@ app.post('/jsnlog.logger', (req, res) => {
 // Express Validator Middleware
 // @see https://github.com/VojtaStavik/GetBack2Work-Node/blob/master/node_modules/express-validator/README.md
 app.use(validator({
-  errorFormatter: function(param, msg, value) {
+  errorFormatter: function (param, msg, value) {
     var namespace = param.split('.'),
-        root    = namespace.shift(),
-        formParam = root;
+      root = namespace.shift(),
+      formParam = root;
 
-    while(namespace.length) {
+    while (namespace.length) {
       formParam += '[' + namespace.shift() + ']';
     }
     return {
-      param : formParam,
-      msg   : msg,
-      value : value
+      param: formParam,
+      msg: msg,
+      value: value
     };
   }
 }));
 
 app.use(cookieParser());
 app.use(logger('dev'));
-
-var token = require('./config/token').token;
 
 // Express Session Middleware
 // @see https://github.com/expressjs/session
@@ -129,7 +98,7 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('*', function(req, res, next){
+app.get('*', function (req, res, next) {
   res.locals.user = req.user || null;
   //console.log('user', res.locals.user);
   next();
@@ -140,14 +109,16 @@ let indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 let impressumRouter = require('./routes/impressum');
 app.use('/impressum', impressumRouter);
+let testpageRouter = require('./routes/testpage');
+app.use('/testpage', testpageRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -156,5 +127,46 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Twitter
+var Twit = require('twit');
+var TwitClient = new Twit({
+  consumer_key: token.twitter_consumer_key,
+  consumer_secret: token.twitter_consumer_secret,
+  access_token: token.twitter_access_token,
+  access_token_secret: token.twitter_access_token_secret
+});
+
+//TwitClient.post('statuses/update', { status: "Hello there! We are Salus Solutions." });
+
+/* function connectMongoDB() {
+  (async () => {
+    // set up default ("Docker") mongoose connection
+    await mongoose.connect(config.databaseDocker, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      autoReconnect: true
+    }).then(db => {
+      console.log('Connected to MongoDB (databasename: "' + db.connections[0].name + '") on host "' + db.connections[0].host + '" and on port "' + db.connections[0].port + '""');
+    }).catch(async err => {
+      console.log('Connection to ' + config.databaseDocker + ' failed, try to connect to ' + config.databaseLocal);
+      // set up "local" mongoose connection
+      await mongoose.connect(config.databaseLocal, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        autoReconnect: true
+      }).then(db => {
+        console.log('Connected to MongoDB (databasename: "' + db.connections[0].name + '") on host "' + db.connections[0].host + '" and on port "' + db.connections[0].port + '""');
+      }).catch(err2nd => {
+        console.log('Error at MongoDB-connection with Docker: ' + err);
+        console.log('Error at MongoDB-connection with Localhost: ' + err2nd);
+        console.log('Retry to connect in 3 seconds');
+        setTimeout(connectMongoDB, 3000); // retry until db-server is up
+      });
+    });
+  })();
+}
+// connect to MongoDB
+connectMongoDB(); */
 
 module.exports = app;
