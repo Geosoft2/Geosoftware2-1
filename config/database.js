a64b314140121e11741ab9e06e4ba0e97479d15e
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2019-11-20 16:27:30 +0100

    Removed node_modules

--- config/database.js
+++
@@ -1,8 +0,0 @@
-// jshint esversion: 6
-// jshint node: true
-"use strict";
-
-module.exports = {
-  databaseDocker:'mongodb://mongodbservice/itemdb',
-  databaseLocal:'mongodb://localhost:27017/itemdb'
-};
---
+++ config/token.js
@@ -0,0 +1,15 @@
+// jshint esversion: 6
+// jshint node: true
+"use strict";
+
+if (typeof exports == "undefined") {
+    var exports = window;
+}
+
+module.exports = {
+    secretSession: 'ExampleString',
+    twitter_consumer_key: 'KIbQWCbiPYbwFvbLshAVTeEmd',
+    twitter_consumer_secret: 'G3CHnto8NhvvKoz285TJ7zNHxN8UQkuXuGnB6WphV4mt9WYc6R',
+    twitter_access_token: '1194921559837827072-IqMc1AqO0CGBNqJB30nAkHwEvG3axe',
+    twitter_access_token_secret: 'u9ZPxYy0FYponsj1j4Le1xH1NCBBNmLa01xaFYJBl5bDN'
+};
