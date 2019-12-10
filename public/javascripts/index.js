b7fd9d8492f1a816c72682e21bec6064518433b4
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2019-12-10 23:30:30 +0100

    Geotagged Tweets show up as popups on map
    - Tweets now show up as popups on the map
    - Tweets now get filtered before visualizing them by removing non geotagged ones

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,3 @@
-//TEST
-
 $(document).ready(() => {
     document.cookie = "geocode=";
     $('select').selectpicker();
@@ -36,13 +34,42 @@ function requestTweets() {
 
 function processData(data) {
     $(".tweet").remove();
-    var tweets = data.statuses;
+    var raw = data.statuses;
+    var filtered = new Array();
 
-    tweets.forEach((tweet) => {
-        var tweet_id = tweet.id_str;
-        var tweet_html = '<div class="tweet carousel-item" id="' + tweet_id + '"></div>';
-        $("#tweet_carousel_inner").append(tweet_html);
-        var tweet_dom = $("#" + tweet_id)[0];
-        twttr.widgets.createTweet(tweet_id, tweet_dom, widget_config);
+    raw.forEach((tweet) => {
+        if ((tweet.coordinates != null) || (tweet.geo != null) || (tweet.place != null)) {
+            filtered.push(tweet);
+        }
+    });
+
+    filtered.forEach((tweet) => {
+        console.log(tweet);
+        drawTweetToUI(tweet);
+        var geo = tweet.geo;
+        var place = tweet.place;
+        if ((geo != null) && (geo != undefined)) {
+            L.marker(geo.coordinates).addTo(map);
+        } else {
+            if ((place != null) && (place != undefined)) {
+                var bbox = place.bounding_box;
+                var coordinates = turf.flip((turf.center(bbox))).geometry.coordinates;
+                L.marker(coordinates, { id: tweet.id_str })
+                    .addTo(map)
+                    .on("click", (e) => {
+                        var id = e.target.options.id;
+                        console.log(id);
+                    });
+            }
+        }
     });
 };
+
+function drawTweetToUI(tweet) {
+    var tweet_id = tweet.id_str;
+    var tweet_html = '<div class="tweet carousel-item" id="' + tweet_id + '"></div>';
+    $("#tweet_carousel_inner").append(tweet_html);
+    var tweet_dom = $("#" + tweet_id)[0];
+    twttr.widgets.createTweet(tweet_id, tweet_dom, widget_config);
+};
+
