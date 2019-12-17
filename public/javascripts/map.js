a9c49b9d4714fc8e3cb7ec5fda751043e9a215d6
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2019-12-17 18:08:07 +0100

    UI overhaul 1

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -245,6 +245,26 @@ function map() {
         })
         .addTo(map);
 
+    //Custom Controls
+    var custom = L.Control.extend({
+        options: {
+            position: "bottomleft"
+        },
+        onAdd: function (map) {
+            var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom');
+
+            container.style.backgroundColor = 'white';
+            container.style.width = '30px';
+            container.style.height = '30px';
+
+            container.onclick = function () {
+                saveCookie();
+            }
+            return container;
+        }
+    });
+
+    map.addControl(new custom());
 }
 
 // Funktion um die einzelnen Landkreise farblich korrekt darzustellen. Die Farbe ist anhängig
