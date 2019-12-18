8f4f9f0f57ccd8b6fd295cae188d87ce9ce27eec
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2019-12-18 13:58:25 +0100

    Tweetanzeige Test

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,7 +7,7 @@ function requestTweets() {
         var query = {
             keyword: $("#keyword_input").val(),
             geocode: "37.735997,-118.427388,100km",
-            language: "",
+            language: $("#language_select").val(),
             locale: "ja",
             result_type: "recent",
             count: "100",
@@ -34,14 +34,15 @@ function requestTweets() {
             url: 'http://localhost:3000/tweetdb',
         }).done(function (data) {
             console.log('Success: Tweets from the database received');
-            filterTweets(data);
+            //filterTweets(data);
+            test(data);
         }).fail(function (xhr, status, error) {
             console.log('Error: ' + error);
         });
     });
 };
 
-function getPolygonsInBbox() {
+/* function getPolygonsInBbox() {
     var bboxsouthWest_lat = parseFloat(getCookie("bboxsouthWest_lat"));
     var bboxsouthWest_lng = parseFloat(getCookie("bboxsouthWest_lng"));
     var bboxnorthEast_lat = parseFloat(getCookie("bboxnorthEast_lat"));
@@ -92,60 +93,60 @@ function getPolygonsInBbox() {
 
 //TODO: Ueberpruefe, ob Tweet im Polygon liegt
 function polygonContainsTweet(polygon, tweet) {
+    //console.log(polygon, tweet);
 
+    if (tweet.geo != null) {
+        var coord = tweet.geo.coordinates;
+        console.log(turf.booleanPointInPolygon(coord, polygon));
+    } else {
+        if (tweet.place != null) {
+            //console.log(tweet.place);
+        }
+    }
 }
 
 function filterTweets(tweets) {
     var polygons = getPolygonsInBbox();
 
+    var testpolygon = {
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [[
+                [7.5146484375, 52.29504228453735],
+                [7.84423828125, 50.708634400828224],
+                [10.61279296875, 50.233151832472245],
+                [13.095703125, 51.97134580885172],
+                [10.349121093749998, 53.173119202640635],
+                [7.5146484375, 52.29504228453735]
+            ]]
+        }
+    };
+
+    polygons.push(testpolygon);
+
+    //console.log(polygons);
+
     var filteredtweets = [];
 
     polygons.forEach((polygon) => {
-        tweets.forEach((tweet) => {
+        tweets.forEach((t) => {
+            var tweet = JSON.parse(t.tweet);
             if (polygonContainsTweet(polygon, tweet)) {
-                filteredtweets.push(tweet);
+                filteredtweets.push();
             }
         });
     });
-}
+} */
 
-/* function processData(data) {
+function test(tweets) {
     $(".tweet").remove();
-    var raw = data.statuses;
-    var filtered = new Array();
-
-    raw.forEach((tweet) => {
-        if ((tweet.coordinates != null) || (tweet.geo != null) || (tweet.place != null)) {
-            filtered.push(tweet);
-        }
-    });
-
-    filtered.forEach((tweet) => {
-        console.log(filtered);
+    tweets.forEach((t) => {
+        var tweet = JSON.parse(t.tweet);
         drawTweetToUI(tweet);
-        var geo = tweet.geo;
-        var place = tweet.place;
-        var marker = null;
-        if ((geo != null) && (geo != undefined)) {
-            marker = L.marker(geo.coordinates);
-        } else {
-            if ((place != null) && (place != undefined)) {
-                var bbox = place.bounding_box;
-                var icon = L.AwesomeMarkers.icon({
-                    markerColor: 'blue',
-                });
-                var coordinates = turf.flip((turf.center(bbox))).geometry.coordinates;
-                marker = L.marker(coordinates, { id: tweet.id_str, icon: icon });
-            }
-        }
-        marker.on("click", (e) => {
-            var id = e.target.options.id;
-            $('.carousel-item').removeClass("active");
-            $('#' + id).addClass("active");
-        });
+        drawTweetToMap(tweet);
     });
     $('.carousel-item').first().addClass("active");
-};
+}
 
 function drawTweetToUI(tweet) {
     var tweet_id = tweet.id_str;
@@ -153,4 +154,7 @@ function drawTweetToUI(tweet) {
     $("#tweet_carousel_inner").append(tweet_html);
     var tweet_dom = $("#" + tweet_id)[0];
     twttr.widgets.createTweet(tweet_id, tweet_dom, widget_config);
-}; */
+};
+
+function drawTweetToMap(tweet) {
+}
