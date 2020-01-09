6036f872f38992ebdbaf56b5f943a92ff0424d6a
ConstantinRUM <45095307+ConstantinRUM@users.noreply.github.com>
2020-01-09 11:11:08 +0100

    dwd_new

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@ const APIController = require('../controllers/api');
 // renders the documentation page
 router.get('/', APIController.getApi);
 router.get('/twitter', APIController.getApi);
-router.get('/instagram', APIController.getApi);
+//router.get('/instagram', APIController.getApi);
 router.get('/dwd', APIController.getApi);
 
 module.exports = router;
