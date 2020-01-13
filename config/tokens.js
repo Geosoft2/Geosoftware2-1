8c890c4e3450bb5bc3b7f9f03113942432ca9733
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2020-01-13 13:56:51 +0100

    Update tokens.js

--- config/tokens.js
+++ config/tokens.js
@@ -8,9 +8,9 @@ if (typeof exports == "undefined") {
 
 module.exports = {
     secretSession: 'ExampleString',
-    twitter_consumer_key: 'KIbQWCbiPYbwFvbLshAVTeEmd',
-    twitter_consumer_secret: 'G3CHnto8NhvvKoz285TJ7zNHxN8UQkuXuGnB6WphV4mt9WYc6R',
-    twitter_access_token: '1194921559837827072-IqMc1AqO0CGBNqJB30nAkHwEvG3axe',
-    twitter_access_token_secret: 'u9ZPxYy0FYponsj1j4Le1xH1NCBBNmLa01xaFYJBl5bDN',
-    mapbox_accessToken: 'pk.eyJ1IjoibWdhZG8wMSIsImEiOiJjazQxaHZvZTcwMWdqM2RvYmF4eWRzZ2diIn0.z3YweqsFFX-KbTYMRmz_AA'
+    twitter_consumer_key: '',
+    twitter_consumer_secret: '',
+    twitter_access_token: '',
+    twitter_access_token_secret: '',
+    mapbox_accessToken: ''
 };
